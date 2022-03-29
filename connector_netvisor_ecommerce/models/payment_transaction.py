c5171738d066fca36dd761ef3c3bd77fd41f822c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-03-29 16:10:55 +0300

    [IMP] Update status to Netvisor after creating payment

--- connector_netvisor_ecommerce/models/payment_transaction.py
+++ connector_netvisor_ecommerce/models/payment_transaction.py
@@ -6,7 +6,7 @@ class PaymentTransaction(models.Model):
     _inherit = "payment.transaction"
 
     def _invoice_sale_orders(self):
-        res = super(PaymentTransaction, self)._invoice_sale_orders()
+        res = super()._invoice_sale_orders()
 
         if self.env["ir.config_parameter"].sudo().get_param("sale.automatic_invoice"):
             for trans in self.filtered(lambda t: t.sale_order_ids):
@@ -15,5 +15,11 @@ class PaymentTransaction(models.Model):
                         "netvisor_delayed_send": True,
                     }
                 )
+
+        return res
+
+    def _create_payment(self, add_payment_vals={}):
+        res = super()._create_payment(add_payment_vals)
+        self.invoice_ids.action_netvisor_export_status()
 
         return res

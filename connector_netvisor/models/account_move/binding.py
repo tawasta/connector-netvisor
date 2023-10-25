6113d28d3f3e2ee0b826aafa2cf314a979d2ebbb
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-10-25 16:48:13 +0300

    [FIX] Updating Netvisor status for invoices

--- connector_netvisor/models/account_move/binding.py
+++ connector_netvisor/models/account_move/binding.py
@@ -90,7 +90,7 @@ class NetvisorInvoice(models.Model):
                 return
             elif record.state == "cancel":
                 # Cancelled invoices should not be opened by Netvisor, nothing to do
-                return
+                pass
             elif invoice_status == "unsent":
                 # Set to draft
                 # Generally we don't want Netvisor to reset invoice to draft,
@@ -100,7 +100,7 @@ class NetvisorInvoice(models.Model):
             elif invoice_status == "paid":
                 if record.payment_state in ["paid", "reversed"]:
                     # Already paid, nothing to do
-                    return
+                    record.netvisor_status = invoice_status
 
                 # Set invoice as fully paid
                 # TODO: get correct payment date
@@ -115,7 +115,7 @@ class NetvisorInvoice(models.Model):
                 payment_amount = record.amount_residual
 
                 # This will currently set today as payment date
-                # It is usually incorrect but we don't have the correct data here
+                # It is usually incorrect, but we don't have the correct data here
                 payment_date = datetime.date.today()
 
                 payment_values = {

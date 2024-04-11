348586f9797de88f4217a4702049a31a941ae744
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-04-11 10:41:23 +0300

    [REF] Formatting

--- connector_netvisor/models/account_move/exporter.py
+++ connector_netvisor/models/account_move/exporter.py
@@ -57,7 +57,6 @@ class NetvisorInvoiceExportMapper(Component):
             "connector_netvisor.netvisor_salesinvoice",
             {"invoice": record, "backend": backend},
         )
-        print(xml_string)
 
         binding_model = self.env["netvisor.invoice"]
         _logger.debug(f"Using XML string {xml_string}")

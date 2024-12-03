c2355f4c4c98e07f98c8d4bd591850fb3da5eeab
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-12-03 08:52:49 +0200

    [REF] Formatting

--- connector_netvisor/models/netvisor_backend.py
+++ connector_netvisor/models/netvisor_backend.py
@@ -111,7 +111,8 @@ class NetvisorBackend(models.Model):
         "lower boundary for the invoice date. This field gets "
         "automatically updated after a successful fetch.",
         default="2020-01-01 00:00:00",
-        
+    )
+
     customer_invoice_use_delivery_address = fields.Boolean(
         string="Use delivery address",
         help="Send customer invoice delivery address to Netvisor",

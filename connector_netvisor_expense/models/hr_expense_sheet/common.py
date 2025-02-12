7c29f6e6d4e8b51e738649db9a4c1ecb40319c55
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-02-12 15:16:24 +0200

    [IMP] Don't create vendor bills for expenses

--- connector_netvisor_expense/models/hr_expense_sheet/common.py
+++ connector_netvisor_expense/models/hr_expense_sheet/common.py
@@ -16,3 +16,24 @@ class HrExpenseSheet(models.Model):
             record.expenses_sent_to_netvisor = True
             msg = _("Expense report posted to Netvisor")
             record.message_post(body=msg)
+
+    def _do_create_moves(self):
+        # Don't create moves when using Netvisor expenses.
+        # Netvisor will handle the payment
+        self.set_to_posted()
+
+        # Return empty recordset
+        moves = self.env["account.move"]
+        return moves
+
+    def _do_reverse_moves(self):
+        # Don't create reverse moves when using Netvisor expenses
+        return
+
+    def _prepare_bills_vals(self):
+        res = super()._prepare_bills_vals()
+
+        # Don't send bills to Netvisor
+        res["netvisor_send"] = False
+
+        return res
